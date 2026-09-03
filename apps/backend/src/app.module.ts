03697a51eb2a426bd7fb71e37f9f9595514cdd80
jonny2012 <vanea.ss21@gmail.com>
2026-09-03 20:34:48 +0200

    add CRUD for user
    [+] findAll, findByID requests

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.ts';
 import { AppService } from './app.service.ts';
 import { PrismaModule } from './prisma/prisma.module.ts';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module.ts';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
+      envFilePath: './../.env',
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
