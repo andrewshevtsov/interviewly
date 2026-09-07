7adaa279024848f4e3a17e2df53922d63d71c6cf
jonny2012 <vanea.ss21@gmail.com>
2026-09-07 14:50:56 +0200

    hotfix:Dockerizing backend and frontend
    [+] Added Dockerfile to backend and edited frontend Dockerfile
    
    [-] Deleted unuzed packages from package json
    
    [+] Edited docker compose file to work in dev mode

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.ts';
 import { AppService } from './app.service.ts';
 import { PrismaModule } from './prisma/prisma.module.ts';
+import { ConfigModule } from "@nestjs/config"
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule,
+    ConfigModule.forRoot({
+      envFilePath: '../../.env',
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
